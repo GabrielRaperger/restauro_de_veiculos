5de1d1f27f02b30302c17ce2a991e3732a8488db
Gabriel <bielraperger@gmail.com>
2024-08-02 11:14:56 +0100

    organização das pastas

--- Projeto/projeto_bd2/app_bd2/views.py
+++ Projeto/projeto_bd2/app_bd2/views.py
@@ -5,13 +5,13 @@ def home(request):
     return render(request, 'admin/home.html', {'page_title': 'Home'})
 
 def clientes(request):
-    return render(request,'admin/clientes.html', {'page_title': 'Home'})
+    return render(request,'cliente/lista_clientes.html', {'page_title': 'Home'})
 
 def faturas(request):
-    return render(request, 'admin/faturas.html')
+    return render(request, 'faturas/lista_faturas.html')
 
 def reparacoes(request):
-    return render(request, 'admin/reparacoes.html')
+    return render(request, 'reparacoes/lista_reparacoes.html')
 
 def veiculos(request):
-    return render(request, 'admin/veiculos.html')
+    return render(request, 'veiculos/lista_veiculos.html')
