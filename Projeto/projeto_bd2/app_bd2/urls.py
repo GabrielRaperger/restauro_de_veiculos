f03af6f84ef6ea34587d818f03a9b82ba0c4aa66
F0liveira <lopesoliveirafabio@hotmail.com>
2024-08-12 15:42:03 +0100

    Adicionar cliente

--- Projeto/projeto_bd2/app_bd2/urls.py
+++ Projeto/projeto_bd2/app_bd2/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path
 
-from .views import clientes, dashboard, lista_MaoDeObra, lista_faturas, reparacoes, veiculos, ver_MaoDeObra, ver_faturas
+from .views import adicionar_cliente, clientes, dashboard, lista_MaoDeObra, lista_faturas, reparacoes, veiculos, ver_MaoDeObra, ver_faturas
 
 app_name = 'app_bd2'
 
 urlpatterns = [
     path('', dashboard, name='dashboard'),
     path('clientes/', clientes, name='clientes'),
+    path('clientes/adicionar_cliente/', adicionar_cliente, name='adicionar_cliente'),
     path('faturas/', lista_faturas, name='lista_faturas'),
     path('faturas/<int:id_faturas>/', ver_faturas, name='ver_faturas'),
     path('mao_obra/', lista_MaoDeObra, name='lista_MaoDeObra'),
--- Projeto/projeto_bd2/app_bd2/views.py
+++ Projeto/projeto_bd2/app_bd2/views.py
@@ -1,14 +1,37 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from django.shortcuts import render, get_object_or_404
+from django.db import connection
+
+from .forms.cliente import ClienteForm
 
 from .models import Faturas, MaoDeObra
 
-# Create your views here.
+
 def dashboard(request):
     return render(request, 'dashboard.html', {'page_title': 'Dashboard'})
 
 def clientes(request):
     return render(request,'clientes/lista_clientes.html', {'page_title': 'Home'})
+
+def adicionar_cliente(request):
+    if request.method == 'POST':
+        form = ClienteForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data['username']
+            first_name = form.cleaned_data['first_name']
+            last_name = form.cleaned_data['last_name']
+            email = form.cleaned_data['email']
+            password = form.cleaned_data['password']
+            group_name = 'Cliente' 
+
+            with connection.cursor() as cursor:
+                cursor.execute("CALL proc_inserir_cliente(%s, %s, %s, %s, %s, %s);", 
+                               [username, first_name, last_name, email, password, group_name])
+            
+            return redirect('app_bd2:clientes')  
+    else:
+        form = ClienteForm()
+    return render(request, 'clientes/adicionar_cliente.html', {'form': form})
 
 def lista_faturas(request):
     return render(request, 'faturas/lista_faturas.html')
