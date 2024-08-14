8f41a8e6c1024507af8a8d0c5363a28bf02b6e46
F0liveira <lopesoliveirafabio@hotmail.com>
2024-08-14 15:11:07 +0100

    Mostrar nome do utilizador no menu

--- Projeto/projeto_bd2/login_bd2/management/commands/setup_users.py
+++ Projeto/projeto_bd2/login_bd2/management/commands/setup_users.py
@@ -1,10 +1,10 @@
 from django.core.management.base import BaseCommand
 from django.contrib.auth.models import User, Group
-from django.core.management import call_command
 from django.db import connection
+from django.core.management import call_command
 
 class Command(BaseCommand):
-    help = 'Aplicar migração, criar superuser, criar os restantes utilizadores e grupos, e configurar chave estrangeira'
+    help = 'Aplicar migração, criar superuser, criar os restantes utilizadores e grupos'
 
     def handle(self, *args, **options):
         # Aplicar migração
@@ -12,10 +12,23 @@ class Command(BaseCommand):
         call_command('migrate')
         self.stdout.write(self.style.SUCCESS('Migração aplicada com sucesso'))
 
+        # Adicionar coluna user_id se não existir
+        self._add_user_id_column()
+
+        # Criar a chave estrangeira entre auth_user e usuarios
+        self._create_foreign_key()
+
         # Criar Superuser
         self.stdout.write('A criar um superuser...')
         if not User.objects.filter(username='admin').exists():
-            User.objects.create_superuser('admin', 'admin@django.com', 'adminpassword')
+            admin_user = User.objects.create_superuser(
+                username='admin',
+                email='admin@django.com',
+                password='adminpassword',
+                first_name='Admin',
+                last_name='User'
+            )
+            self._create_usuario_entry(admin_user, 'Admin User', '999999999', '123456789', 'Admin Address')
             self.stdout.write(self.style.SUCCESS('Superuser criado com sucesso'))
         else:
             self.stdout.write(self.style.WARNING('Superuser já existe'))
@@ -33,33 +46,78 @@ class Command(BaseCommand):
         # Criar Utilizadores
         self.stdout.write('A criar utilizadores...')
         users_info = [
-            ('cliente', 'cliente@django.com', 'clientepassword', 'Cliente'),
-            ('trabalhador', 'trabalhador@django.com', 'trabalhadorpassword', 'Trabalhador')
+            ('cliente', 'cliente@django.com', 'clientepassword', 'Cliente', 'Client', '999888777', '123456789', 'Client Address'),
+            ('trabalhador', 'trabalhador@django.com', 'trabalhadorpassword', 'Worker', 'Work', '111222333', '987654321', 'Worker Address')
         ]
 
-        for username, email, password, group_name in users_info:
+        for username, email, password, first_name, last_name, nif, telemovel, endereco in users_info:
             if not User.objects.filter(username=username).exists():
-                user = User.objects.create_user(username, email, password)
+                user = User.objects.create_user(
+                    username=username,
+                    email=email,
+                    password=password,
+                    first_name=first_name,
+                    last_name=last_name
+                )
+                group_name = 'Cliente' if 'cliente' in username else 'Trabalhador'
                 group = Group.objects.get(name=group_name)
                 user.groups.add(group)
+                self._create_usuario_entry(user, f'{first_name} {last_name}', nif, telemovel, endereco)
                 self.stdout.write(self.style.SUCCESS(f'Utilizador "{username}" criado e adicionado ao grupo "{group_name}"'))
             else:
                 self.stdout.write(self.style.WARNING(f'Utilizador "{username}" já existe'))
 
-        # Adicionar chave estrangeira entre auth_user e usuarios
-        self.stdout.write('A adicionar chave estrangeira entre auth_user e usuarios...')
+        self.stdout.write(self.style.SUCCESS('Script executado com sucesso'))
+
+    def _add_user_id_column(self):
+        with connection.cursor() as cursor:
+            cursor.execute("""
+                DO $$
+                BEGIN
+                    -- Verificar se a coluna user_id já existe
+                    IF NOT EXISTS (
+                        SELECT 1 
+                        FROM information_schema.columns 
+                        WHERE table_name = 'usuarios' 
+                        AND column_name = 'user_id'
+                    ) THEN
+                        -- Adicionar a coluna user_id
+                        ALTER TABLE usuarios
+                        ADD COLUMN user_id INTEGER;
+                    END IF;
+                END $$;
+            """)
+            self.stdout.write(self.style.SUCCESS('Coluna user_id adicionada com sucesso, se não existia'))
+
+    def _create_foreign_key(self):
+        with connection.cursor() as cursor:
+            cursor.execute("""
+                DO $$
+                BEGIN
+                    -- Verificar se a chave estrangeira já existe
+                    IF NOT EXISTS (
+                        SELECT 1 
+                        FROM information_schema.table_constraints 
+                        WHERE constraint_type = 'FOREIGN KEY' 
+                        AND table_name = 'usuarios' 
+                        AND constraint_name = 'usuarios_user_id_fkey'
+                    ) THEN
+                        -- Adicionar a chave estrangeira
+                        ALTER TABLE usuarios
+                        ADD CONSTRAINT usuarios_user_id_fkey
+                        FOREIGN KEY (user_id)
+                        REFERENCES auth_user(id);
+                    END IF;
+                END $$;
+            """)
+            self.stdout.write(self.style.SUCCESS('Chave estrangeira entre auth_user e usuarios criada com sucesso'))
+
+    def _create_usuario_entry(self, user, nome, nif, telemovel, endereco):
         with connection.cursor() as cursor:
             try:
-                cursor.execute("""
-                    ALTER TABLE usuarios
-                    ADD COLUMN user_id INTEGER UNIQUE,
-                    ADD CONSTRAINT fk_user_id
-                    FOREIGN KEY (user_id)
-                    REFERENCES auth_user(id)
-                    ON DELETE CASCADE;
-                """)
-                self.stdout.write(self.style.SUCCESS('Chave estrangeira criada com sucesso'))
+                cursor.execute(
+                    "INSERT INTO usuarios (nome, nif, telemovel, endereco, email, user_id) VALUES (%s, %s, %s, %s, %s, %s)",
+                    [nome, nif, telemovel, endereco, user.email, user.id]
+                )
             except Exception as e:
-                self.stdout.write(self.style.ERROR(f'Erro ao criar chave estrangeira: {e}'))
-
-        self.stdout.write(self.style.SUCCESS('Script executado com sucesso'))
+                self.stdout.write(self.style.ERROR(f'Erro ao inserir dados na tabela usuarios: {e}'))
