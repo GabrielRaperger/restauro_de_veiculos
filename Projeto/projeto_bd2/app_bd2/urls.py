4ce8027d1d10ddd56e68c727750ca117de00669d
F0liveira <lopesoliveirafabio@hotmail.com>
2024-08-19 17:47:55 +0100

    Urls para encarregados

--- Projeto/projeto_bd2/app_bd2/urls.py
+++ Projeto/projeto_bd2/app_bd2/urls.py
@@ -7,15 +7,19 @@ app_name = 'app_bd2'
 
 urlpatterns = [
     path('', dashboard, name='dashboard'),
+    # ---------- CLIENTES ----------------- #
     path('clientes/', clientes, name='clientes'),
     path('clientes/adicionar_cliente/', adicionar_cliente, name='adicionar_cliente'),
     path('clientes/<int:id>/ver_cliente/', views.ver_cliente, name='ver_cliente'),
+    # ---------- ENCARREGADOS ----------------- #
+    path('encarregados/', encarregados, name='encarregados'),
+    path('clientes/adicionar_encarregado/', adicionar_encarregado, name='adicionar_encarregado'),
+    path('clientes/<int:id>/ver_encarregado/', views.ver_encarregado, name='ver_encarregado'),
     # ---------- FATURAS ----------------- #
     path('faturas/', lista_faturas, name='lista_faturas'),
     path('faturas/<int:id_faturas>/', ver_faturas, name='ver_faturas'),
     path('faturas/saida', lista_saida, name='lista_saida'),
     path('criar_fatura/<int:id_saida>/', views.criar_fatura, name='criar_fatura'),
-
     #------------------- MÃO DE OBRA ----------------------#
     path('mao_obra/', lista_MaoDeObra, name='lista_MaoDeObra'),
     path('mao-de-obra/<int:id_mao_de_obra>/', views.ver_mao_de_obra, name='ver_mao_de_obra'),
